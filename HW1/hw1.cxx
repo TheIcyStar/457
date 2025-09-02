24e308d0fdbea79df49cce11b890f986a4006d8b
TheIcyStar <13933661+TheIcyStar@users.noreply.github.com>
2025-09-02 01:33:17 +0000

    HW1 - Required revisions

--- HW1/hw1.cxx
+++ HW1/hw1.cxx
@@ -1,19 +1,23 @@
-/*
-    CMPSC 457 - Section 001
-    Alexander Petrov
-    aop5448
-
-        ===== Written section =====
+/*********************************************************************
+ *  CMPSC 457 Section 001                                            *
+ *  Homework 1                                                       *
+ *                                                                   *
+ *  Alexander Petrov                                                 *
+ *  aop5448                                                          *
+ *                                                                   *
+ *  09/01/2025                                                       *
+ *********************************************************************
     The given code section creates a black-and-magenta checkerboard pattern with 8x8 pixel squares.
 
 
-        == Pattern explalnation ==
+    *****  Pattern explalnation *****
+    On the first line:
         int col = ((w & 0x08) == 0) ^ (h & 0x08) == 0);
     the operands of the bitwise XOR (^) evaluate to true for the first 8 positions, and false for the next 8.
     Then, col will either be 0 or 1 from the XOR operator, so that every other square will be colored.
 
 
-        == Color explalnation ==
+    ***** Color explalnation *****
     For the color components, this line:
     raster[h][w].r = static_cast<float>(col);
     will set the red value of the pixel in the raster to 1 when the square is to be colored
@@ -27,10 +31,9 @@
     will set the green blue value of the pixel in the raster to 1 when the square is to be colored
     as 0x1 & 0x11 is 1.
 
-        == Changing the pattern size ==
+    ***** Changing the pattern size *****
     Simply change the `0x08` on the first line to another hexadecimal number to alter the size of each square.
-
-*/
+ *********************************************************************/
 
 #include <iostream>
 #include <fstream>
@@ -38,20 +41,13 @@
 #include <cmath>
 #include <cstdlib>
 
-struct Color3F {
+struct RGB {
     float r;
     float g;
     float b;
 };
 
-struct Color3Int {
-    int r;
-    int g;
-    int b;
-};
-
 int cStrToInt(char* cstr);
-Color3Int generatePixel(int x, int y);
 
 int main(int argc, char** argv) {
     int x;
@@ -60,6 +56,7 @@ int main(int argc, char** argv) {
     std::string filename;
     std::ofstream outFile;
 
+    //Read and sanity check arguments
     if(argc != 4){
         std::cerr << "Expected 3 arguments, received " << argc - 1 << ".\n" <<
                      "Usage: hw1 [width] [height] [filename]\n";
@@ -78,19 +75,38 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    //Initialize raster
+    //x = w; y = h
+    RGB** raster = new RGB*[y]; //RGB raster[y][x] causes -Wvla
+    for(int yPos=0; yPos < y; yPos++){
+        raster[yPos] = new RGB[x];
+
+        for(int xPos=0; xPos < x; xPos++){
+            int col = ((xPos & 0x08) == 0) ^ ((yPos & 0x08) == 0);
+
+            raster[yPos][xPos].r = static_cast<float>(col);
+            raster[yPos][xPos].g = static_cast<float>(col & 0x00);
+            raster[yPos][xPos].b = static_cast<float>(col & 0x11);
+        }
+    }
+
+
+    //File output
     outFile.open(filename.c_str());
     outFile << "P3\n" <<
                x << " " << y << "\n" <<
                "255\n";
 
-    for(int yPos=0; yPos < y; yPos++){
+    for(int yPos=y-1; yPos >= 0; yPos--){ //Flip the Y axis
         for(int xPos=0; xPos < x; xPos++){
-            Color3Int curPixel = generatePixel(xPos, yPos);
-            outFile << curPixel.r << " " << curPixel.g << " " << curPixel.b << " ";
+            outFile << static_cast<int>(raster[yPos][xPos].r * 255) << " " <<
+                       static_cast<int>(raster[yPos][xPos].g * 255) << " " <<
+                       static_cast<int>(raster[yPos][xPos].b * 255) << " ";
             outCounter++;
 
             //make sure lines are <70 characters
-            if(outCounter >= 5){ //For simplicity just assume that we print 12 chars for each pixel, eg: "255 255 255 "
+            //For simplicity just assume that we print 12 chars for each pixel, eg: "255 255 255 "
+            if(outCounter >= 5){
                 outFile << "\n";
                 outCounter = 0;
             }
@@ -115,15 +131,4 @@ int cStrToInt(char* cstr){
     }
 
     return value;
-}
-
-Color3Int generatePixel(int x, int y){ //w = x, h = y
-    int col = ((x & 0x08) == 0) ^ ((y & 0x08) == 0);
-    Color3Int pixel;
-
-    pixel.r = 255 * (static_cast<float>(col));
-    pixel.g = 255 * (static_cast<float>(col & 0x00));
-    pixel.b = 255 * (static_cast<float>(col & 0x11));
-
-    return pixel;
 }
