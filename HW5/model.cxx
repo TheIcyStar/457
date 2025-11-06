8c0cb63624aad8bbfb91aeff9af6d31c731549c7
TheIcyStar <13933661+TheIcyStar@users.noreply.github.com>
2025-11-05 20:47:20 -0500

    Improve .obj loading

--- HW5/model.cxx
+++ HW5/model.cxx
@@ -1,27 +1,13 @@
 /*
-    CMPSC 457 Section 001
-    Homework 5
-
     Alexander Petrov
     aop5448
 
-    11/5/25
+    contains load_data() performance improvements, Test models loaded instantly during my testing.
+    There's two implementation of reading the 'f' lines,
+    the first only reads only the vertices
+    and the second reads all vertices/texture/normal values
+    the second implementation is commented out because the texture/normal values are not implemented anyways.
 */
-
-
-
-/*************************************************************************
- *  CMPSC 457                                                            *
- *  Template code for HW 5                                               *
- *                                                                       *
- *                                                                       *
- *  Description:                                                         *
- *                                                                       *
- *  This is an implementation of the class Model                         *
- *  - Currently the bottleneck is load_data()                            *
- *  - If you can improve the performance, please let me know             *
- *                                                                       *
- *************************************************************************/
 
 
 #include <iostream>
@@ -73,7 +59,7 @@ std::vector<int> Model::face(int i)
 // private method
 void Model::load_data(const char *filename)
 {
-    std::cout << "\nloading data (v2)..." << std::flush;
+    std::cout << "\nloading data (v3)..." << std::flush;
 
     std::ifstream in;
     in.open(filename, std::ifstream::in);
@@ -92,8 +78,11 @@ void Model::load_data(const char *filename)
 
     std::istringstream str_in(data);
 
-
+    std::string ignoreStr;
     std::string line;
+    std::string mode;
+    std::vector<int> faceVertexes;
+
     float x_max = -std::numeric_limits<float>::max();
     float y_max = -std::numeric_limits<float>::max();
     float z_max = -std::numeric_limits<float>::max();
@@ -101,67 +90,78 @@ void Model::load_data(const char *filename)
     float y_min =  std::numeric_limits<float>::max();
     float z_min =  std::numeric_limits<float>::max();
 
-    while (!str_in.eof()) {
-        std::getline(str_in, line);
+    while (std::getline(str_in, line)) {
         std::istringstream ss(line.c_str());
-        char ignore;
-        if (!line.compare(0, 2, "v ")) {
-            // .obj format for v: v x y z [w=1.0]
+        mode = "";
+        ss >> mode;
+
+        if(mode == "v"){
+            // vertex definition: "v x y z"
             vec3 v;
-            ss >> ignore;               // consume "v "
-            ss >> v[0] >> v[1] >> v[2]; // read x, y, z
-
-                                        // find the bounding box for vertices
-            x_max = std::max(x_max, v[0]);
-            y_max = std::max(y_max, v[1]);
-            z_max = std::max(z_max, v[2]);
-            x_min = std::min(x_min, v[0]);
-            y_min = std::min(y_min, v[1]);
-            z_min = std::min(z_min, v[1]);
-
+            ss >> v.x >> v.y >> v.z;
             verts.push_back(v);
-        }
-        else if (!line.compare(0, 2, "f ")) {
-            // .obj format for f: f index1 index2 index3
+
+            x_max = std::max(x_max, v.x);
+            y_max = std::max(y_max, v.y);
+            z_max = std::max(z_max, v.z);
+            x_min = std::min(x_min, v.x);
+            y_min = std::min(y_min, v.y);
+            z_min = std::min(z_min, v.z);
+
+        } else if (mode == "f"){
+            std::string tuple;
             std::vector<int> face;
-            int v_idx, t_idx, n_idx;
-            ss >> ignore;               // consume "f "
-            if (regex_search(line, std::regex{ R"(\s\d+//\d+\s)" })) {
-                // .obj format for f: "f 7//1 8//2 9//3"     <= vertex//normal
-                // - currently only vertices are supported
-                while (ss >> v_idx >> ignore >> ignore >> n_idx)
-                    face.push_back(v_idx - 1);  // .obj index starts from 1, not 0
-            }
-            else if (regex_search(line, std::regex{ R"(\s\d+/\d+/\d+\s)" })) {
-                // .obj format for f: "f 6/4/1 3/5/3 7/6/5"  <= vertex/texture/normal
-                // - currently only vertices are supported
-                while (ss >> v_idx >> ignore >> t_idx >> ignore >> n_idx)
-                    face.push_back(v_idx - 1);  // .obj index starts from 1, not 0
-            }
-            else if (regex_search(line, std::regex{ R"(\s\d+/\d+\s)" })) {
-                // .obj format for f: "f 3/1 4/2 5/3"        <= vertex/texture
-                // - currently only vertices are supported
-                while (ss >> v_idx >> ignore >> t_idx)
-                    face.push_back(v_idx - 1);  // .obj index starts from 1, not 0
-            }
-            else if (regex_search(line, std::regex{ R"(\s\d+\s)" })) {
-                // .obj format for f: "f 1 2 3"              <= vertex
-                // - currently only vertices are supported
-                while (ss >> v_idx)
-                    face.push_back(v_idx - 1);  // .obj index starts from 1, not 0
-            }
+            // face definitions
+            // Only vertex data:            "f v1 v2 v3 ..."
+            // Vertex and texture coords:   "f v1/vt1 v2/vt2 v3/vt3 ..."
+            // Vertex, texture, normals:    "f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3 ..."
+            // Vertex and normals:          "f v1//vn1 v2//vn2 v3//vn3 .."
 
-            //faces.push_back(face);
+            //Only supports verticies, read the first float and skip until the next whitespace
+            //*
+            while(ss >> tuple){
+                std::istringstream tuplestream(tuple);
+                std::string part;
 
-            if (face.size() == 3) {
+                int vert;
+                tuplestream >> vert;
+                face.push_back(vert - 1);
+            }
+            //*/
+
+            //All formats
+            //only tested with CMPSC 457's .obj
+            /*
+            while(ss >> tuple){
+                std::istringstream tuplestream(tuple);
+                std::string part;
+                int vert, texture, normal;
+
+                std::getline(tuplestream, part, '/');
+                vert = std::stoi(part);
+
+                if(std::getline(tuplestream, part, '/') && !part.empty()){
+                    texture = std::stoi(part);
+                }
+
+                if(std::getline(tuplestream, part, '/') && !part.empty()){
+                    normal = std::stoi(part);
+                }
+
+                face.push_back(vert - 1);
+            }
+            //*/
+
+            // Triangulate the faces
+            if(face.size() == 3){
                 faces.push_back(face);
-            }
-            else if (face.size() > 3) {
-                for (unsigned int i = 1; i < face.size() - 1; i++)
+
+            } else if(face.size() > 3){
+                for (unsigned int i = 1; i < face.size() - 1; i++) {
                     faces.push_back(std::vector<int>{ face[0], face[i], face[i + 1] });
-            }
-            else {
-                std::cout << "face.size() = " << face.size() << std::endl;
+                }
+            } else {
+                std::cerr << ".obj file contains a face with " << face.size() << " faces, skipping" << std::endl;
             }
         }
     }
